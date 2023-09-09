186d135c22d902a821db30172a0f1bc870e5f124
seop0728 <ms_sh0728@naver.com>
2023-09-09 16:15:06 +0900

    feat: upload.page prompt tag

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,6 +1,3 @@
-const { PutObjectCommand } = require('@aws-sdk/client-s3');
-const s3 = require('../config/aws-config');
-const amqp = require('amqplib');
 const UploadService = require('../services/users.service');
 
 class UploadController {
@@ -8,77 +5,42 @@ class UploadController {
 		this.uploadService = new UploadService();
 	}
 
-sendMQ = async (req, res) => {
-  const userEmail = req.user.email;
-  const userCheck = await this.uploadService.findUser(userEmail);
+	sendMQ = async ( req, res ) => {
+		const userEmail = req.user.email;
+		const userCheck = await this.uploadService.findUser(userEmail);
 
-  if (userCheck.errorMessage) {
-    return res.status(400).json({ errorMessage: userCheck.errorMessage });
-  }
+		if ( userCheck.errorMessage ) {
+			return res.status(400).json({ errorMessage : userCheck.errorMessage });
+		}
 
-  try {
-    const result = await this.uploadService.processRekognition(req);
+		try {
+			const result = await this.uploadService.processRekognition(req);
 
-    if (result.error) {
-      return res.status(400).json({ error: result.error });
-    }
+			if ( result.error ) {
+				return res.status(400).json({ error : result.error });
+			}
 
+			const messageData = {
+				userMail : userEmail,
+				originPhoto : result.uniqueFileName,
+				bgPrompt : result.bgPrompt,
+			};
 
-const messageData = {
-  animalType: result.objectLabel,
-  userMail: userEmail,
-  originPhoto: result.uniqueFileName,
-};
+			await this.uploadService.insertUser(userEmail);
+			await this.uploadService.sendMessage(messageData);
 
-    await this.uploadService.insertUser(userEmail);
-    await this.uploadService.sendMessage(messageData);
-
-    res.status(200).json({ message: 'Message sent successfully.' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'An error occurred while sending the message.' });
-  }
-};
+			res.status(200).json({ message : 'Message sent successfully.' });
+		} catch ( error ) {
+			console.error(error);
+			res.status(500).json({ error : 'An error occurred while sending the message.' });
+		}
+	};
 
 	getUser = async ( req, res ) => {
-    const userEmail = req.user.email;
-    const userCheck = await this.uploadService.getUser(userEmail);
-    res.send(userCheck);
-  }
-
-
-
-//MQ get
-	// test2 = async ( req, res ) => {
-	// 	await this.uploadService.test2();
-	// 	try {
-	// 		res.status(200).json({ message : 'Message sent successfully.' });
-	// 	} catch ( error ) {
-	// 		console.error(error);
-	// 		res.status(500).json({ error : 'An error occurred while sending the message.' });
-	// 	}
-	// };
-
-
-	// uploadFile = async ( req, res ) => {
-	//
-	// 	const file = req.file;
-	// 	if ( !file ) {
-	// 		return res.status(400).send('No file uploaded.');
-	// 	}
-	//
-	// 	try {
-	// 		res.status(200).json({
-	// 			message : 'Image uploaded successfully.',
-	// 			result : file.location,
-	// 		});
-	//
-	// 	} catch ( error ) {
-	// 		console.error(error);
-	// 		res.status(400).json({ error : error.message });
-	// 	}
-	//
-	// };
+		const userEmail = req.user.email;
+		const userCheck = await this.uploadService.getUser(userEmail);
+		res.send(userCheck);
+	};
 
 }
 
